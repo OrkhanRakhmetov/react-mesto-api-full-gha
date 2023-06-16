5c867490954ab440c0e8ef2e6a373f0cbae61b04
OrkhanRakhmetov <orakhmetov@mail.ru>
2023-06-16 20:25:39 +0300

    проектная работа 15

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -6,6 +6,7 @@ const ForbiddenError = require('../errors/ForbiddenError');
 
 module.exports.getCards = (req, res, next) => {
   Card.find({})
+    .populate(['owner', 'likes'])
     .then((cards) => res.status(200).send(cards))
     .catch(next);
 };
